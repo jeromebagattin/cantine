5fd72bc2ec32756dbb8feab6216c6a6f9df19b25
jerome <jerome.bagattin@gmail.com>
2017-01-15 22:55:27 +0100

    debut ajout formulaire menuplats

---
+++ src/CAF/CantineBundle/Form/MenusPlatsType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CAF\CantineBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MenusPlatsType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('lettre')->add('menus')->add('plats')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CAF\CantineBundle\Entity\MenusPlats'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'caf_cantinebundle_menusplats';
+    }
+
+
+}
